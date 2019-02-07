e68751092de50c8a35a60b329d4314e0b9b1b950
Poonam-Vijaywargiya <39735459+Poonam-Vijaywargiya@users.noreply.github.com>
2019-02-08 01:41:45 +0530

    node mode

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    GooglePlus,
+    NativeStorage,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { LoadingController, AlertController, Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+})
+export class Tab2Page {
+
+  constructor(
+    private googlePlus: GooglePlus,
+    private nativeStorage: NativeStorage,
+    public loadingController: LoadingController,
+    private router: Router,
+    private platform: Platform,
+    public alertController: AlertController
+  ) { }
+
+  async doGoogleLogin(){
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+    this.presentLoading(loading);
+    this.googlePlus.login({
+      'scopes': '', // optional - space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+              'offline': true, // Optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+      })
+      .then(user => {
+        //save user data on the native storage
+        this.nativeStorage.setItem('google_user', {
+          name: user.displayName,
+          email: user.email,
+          picture: user.imageUrl
+        })
+        .then(() => {
+           this.router.navigate(["/user"]);
+        }, (error) => {
+          console.log(error);
+        })
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        if(!this.platform.is('cordova')){
+          this.presentAlert();
+        }
+        loading.dismiss();
+      })
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+       message: 'Cordova is not available on desktop. Please try this in a real device or in an emulator.',
+       buttons: ['OK']
+     });
+
+    await alert.present();
+  }
+
+
+  async presentLoading(loading) {
+    return await loading.present();
+  }
+
+}
